1810d9174711f17adcb3475a0ebec1f0d0972e9c
Carlo Sala <carlosalag@protonmail.com>
2021-05-25 13:26:11 +0200

    nearly final version

--- p4/p4.c
+++ p4/p4.c
@@ -10,16 +10,24 @@ double legRec (int, double);
 double chRec (int, double);
 double deriv (int, double(), double);
 void newton (int n, double(), double[n]);
+void coefLeg (int n, double[n], double[n]);
+void coefCh (int n, double[n]);
+double func1 (double);
+double func2 (double);
+double func3 (double);
+double trapezium (int, double, double, double());
+double calInt (int n, double[n], double[n], double());
 
 int main (int argc, char* argv[]) {
   if (argc != 2) return 1;
   int n = atoi(argv[1]);
   if (n < 2) return 1;
-  double legNod[n], chNod[n];
+  double legNod[n], chNod[n], legCo[n], chCo[n];
   newton(n, legRec, legNod);
   newton(n, chRec, chNod);
-  printArray(n, legNod);
-  printArray(n, chNod);
+  coefLeg(n, legNod, legCo);
+  coefCh(n, chCo);
+  return 0;
 }
 
 int sign (double x) {
@@ -60,8 +68,7 @@ double chRec (int n, double x) {
 }
 
 double deriv (int n, double func(), double x) {
-  double r = n * (func(n-1, x) - x * (func(n, x)))/(1 - pow(x, 2));
-  return r;
+  return n * (func(n-1, x) - x * (func(n, x)))/(1 - pow(x, 2));
 }
 
 void newton (int n, double func(), double r[n]) {
@@ -84,4 +91,47 @@ void newton (int n, double func(), double r[n]) {
     sa = s;
   }
   if (n % 2 == 1) r[nn] = 0;
+}
+
+void coefLeg (int n, double nod[n], double r[n]) {
+  for (int i = 0; i < n; i++) {
+    r[i] = 2/((1 - pow(nod[i], 2)) * pow(deriv(n, legRec, nod[i]), 2));
+  }
+}
+
+void coefCh (int n, double r[n]) {
+  for (int i = 0; i < n; i++) {
+    r[i] = M_PI / n;
+  }
+}
+
+double func1 (double x) {
+  return 1./(1 + pow(x, 2));
+}
+
+double func2 (double x) {
+  return pow(x, 8) - 2 * pow(x, 6) + 3 * pow(x, 4) - pow(x, 2) + 5;
+}
+
+double func3 (double x) {
+  return fabs(x);
+}
+
+double trapezium (int n, double a, double b, double func()) {
+  double r = 0;
+  double h = (b - a)/n;
+  double sp = 0;
+  for (int i = 1; i < n; i++) {
+    sp += func(a + i * h);
+  }
+  r = h * (func(a) + func(b) + 2 * sp)/2;
+  return r;
+}
+
+double calInt(int n, double coef[n], double nod[n], double func()) {
+  double r = 0;
+  for (int i = 0; i < n; i++) {
+    r += coef[i] * func(nod[i]);
+  }
+  return r;
 }
